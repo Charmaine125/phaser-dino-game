23992cb8bef7eefb88783f6efcd03cb4fd09247f
Charmaine125 <250098@live.crescent.edu.sg>
2025-03-26 08:58:02 +0000

    until obstacles

--- game.js
+++ game.js
@@ -26,11 +26,15 @@ function preload() {
     this.load.spritesheet("dino", "assets/dino-idle.png", {frameWidth:88, frameHeight:94});
     this.load.image("ground", "assets/ground.png");
     this.load.image("cloud", "assets/cloud.png");
+    for(let i=0; i<7; i++){
+        this.load.image('obstacle${i}', 'assets/cactuses_${i}.png');
+    }}
     
     
 }
 
 function create() {
+    this.player =
     this.physics.add.sprite(200,200,"dino").setOrigin(0);
     this.ground = this.add.tileSprite(0,300,1000,30,"ground").setOrigin(0,1);
     this.clouds = this.add.group();
@@ -41,9 +45,14 @@ function create() {
         ]
     );
     this.gameSpeed =5;
+    this.obstacles = this.physics.add.group({
+        allowGravity:false
+    });
 }
 
 function update() {
 this.ground.tilePositionX +=this.gameSpeed;
 //as game progresses, every time update runs, ground moves by fixed unit to the left 
+const num= Math.floor(Math.random() *6) +1;
+this.obstacles.create(750,220, 'obstacle${num}').setOrigin(0);
 }
